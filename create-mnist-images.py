85fba657289934c6af209611abd4b31ccfe69031
Yoon Gon Park <yoongon.park@gmail.com>
2019-01-26 23:14:08 +0900

    Fix type error

--- create-mnist-images.py
+++ create-mnist-images.py
@@ -5,11 +5,13 @@ from keras import datasets
 
 cifar_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-type = input("Which dataset do you want to generate (1) MNIST 2) CIFAR10)? ")
+type = input('Which dataset do you want to generate (1) MNIST 2) CIFAR10)? ')
 if type == "1":
     (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
 elif type == "2":
     (X_train, y_train), (X_test, y_test) = datasets.cifar10.load_data()
+else:
+    exit()
 
 base_path = os.popen("pwd").read().strip()
 img_folder, label_file = "dataset", "labels.txt"
@@ -26,8 +28,8 @@ img_cnt_per_class = int(input('How many images per class? '))
 
 class_cnt = [0] * 10
 for i in range(X_train.shape[0]):
-    y_idx = y_train[i] if type == 1 else y_train[i][0]
-    y_value = str(y_train[i]) if type==1 else str(cifar_classes[y_train[i][0]])
+    y_idx = int(y_train[i]) if type == '1' else y_train[i][0]
+    y_value = str(y_train[i]) if type=='1' else str(cifar_classes[y_train[i][0]])
     if class_cnt[y_idx] < img_cnt_per_class:
         filename = img_folder + os.sep + y_value + "_" + str(i).zfill(5) + ".png"
 
